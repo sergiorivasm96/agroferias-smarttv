a11c374a93b71afb2a751699946fc9d19eb8da3b
Rene Arias <rene_811@hotmail.com>
2019-10-07 12:18:32 -0500

    modal

--- src/components/mapa/Mapa.js
+++ src/components/mapa/Mapa.js
@@ -1,75 +1,116 @@
-import React from 'react'
-import MapaLugar from './MapaLugar.js'
-import BotonBuscar from './BotonBuscar'
-import MapaImg from './mapa.jpeg'
+import React from 'react';
+import MapaLugar from './MapaLugar.js';
+import BotonBuscar from './BotonBuscar';
+import MapaImg from './mapa.jpeg';
 
-
-class Mapa extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state={
-            tiendas: [
-                {
-                    "idTienda": 1,
-                    "nombre": "Tienda 1",
-                    "descripcion": "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl",
-                    "posicionX": 460,
-                    "posicionY": 150
-                },
-                {
-                    "idTienda": 2,
-                    "nombre": "Tienda 2",
-                    "descripcion": "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl",
-                    "posicionX": 420,
-                    "posicionY": 250
-                },
-                {
-                    "idTienda": 3,
-                    "nombre": "Tienda 3",
-                    "descripcion": "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl",
-                    "posicionX": 760,
-                    "posicionY": 150
-                },
-                {
-                    "idTienda": 4,
-                    "nombre": "Tienda 4",
-                    "descripcion": "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl",
-                    "posicionX": 950,
-                    "posicionY": 150
-                },
-                {
-                    "idTienda": 5,
-                    "nombre": "Tienda 5",
-                    "descripcion": "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl",
-                    "posicionX": 825,
-                    "posicionY": 320
-                },
-            
-            ],
-            popUpVisible: false
+class Mapa extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tiendas: [
+        {
+          idTienda: 1,
+          nombre: 'Tienda 1',
+          descripcion:
+            'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl',
+          posicionX: 460,
+          posicionY: 150
+        },
+        {
+          idTienda: 2,
+          nombre: 'Tienda 2',
+          descripcion:
+            'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl',
+          posicionX: 420,
+          posicionY: 250
+        },
+        {
+          idTienda: 3,
+          nombre: 'Tienda 3',
+          descripcion:
+            'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl',
+          posicionX: 760,
+          posicionY: 150
+        },
+        {
+          idTienda: 4,
+          nombre: 'Tienda 4',
+          descripcion:
+            'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl',
+          posicionX: 950,
+          posicionY: 150
+        },
+        {
+          idTienda: 5,
+          nombre: 'Tienda 5',
+          descripcion:
+            'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec efficitur dolor vitae lobortis varius. Curabitur fermentum leo a eros posuere placerat. Aenean tempor ex lorem, non faucibus diam efficitur a. Etiam vitae imperdiet velit. Curabitur nec cursus orci. Nam nec ex nisl',
+          posicionX: 825,
+          posicionY: 320
         }
-    }
+      ],
+      popUpVisible: false,
+      tiendaModal: {}
+    };
+  }
 
-    handlerClick(tienda)
-    {
-        alert(`${tienda.nombre}: ${tienda.descripcion}`);
-        console.log(`${tienda.nombre}: ${tienda.descripcion}`);
-    }
+  handlerClick(tienda) {
+    this.setState({ tiendaModal: tienda, popUpVisible: true });
+    setTimeout(() => {
+      this.setState({ popUpVisible: false });
+    }, 10000);
+  }
 
-    render (){
-        return(
-            <div>
-                <BotonBuscar></BotonBuscar>
-                <div  id='divGrande' style={{backgroundImage: "url(" + MapaImg + ")", position:'relative', width:'1000px', height:'450px', marginLeft: '4%'}}>
-                    {this.state.tiendas.map((tienda) => (
-                        <div className='item-focusable' style={{left:tienda.posicionX, position:'absolute', top:tienda.posicionY}} onClick={()=>this.handlerClick(tienda)} key={'tienda-' + tienda.idTienda} >
-                        <MapaLugar></MapaLugar>
-                    </div>
-                    ))}
-                </div>
+  render() {
+    return (
+      <div>
+        <BotonBuscar></BotonBuscar>
+        <div
+          id="divGrande"
+          style={{
+            backgroundImage: 'url(' + MapaImg + ')',
+            position: 'relative',
+            width: '1000px',
+            height: '450px',
+            marginLeft: '4%'
+          }}
+        >
+          {this.state.tiendas.map(tienda => (
+            <div
+              className="item-focusable"
+              style={{
+                left: tienda.posicionX,
+                position: 'absolute',
+                top: tienda.posicionY
+              }}
+              onClick={() => this.handlerClick(tienda)}
+              key={'tienda-' + tienda.idTienda}
+            >
+              <MapaLugar></MapaLugar>
             </div>
-           
-        )
-    }
+          ))}
+        </div>
+        <div
+          className="modal-mapa"
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            margin: 'auto',
+            width: '50%',
+            height: '40%',
+            'z-index': 10,
+            'background-color': 'white'
+          }}
+          data-attribute={!this.state.popUpVisible ? 'hidden' : ''}
+          hidden={!this.state.popUpVisible ? 'hidden' : ''}
+        >
+          {this.state.tiendaModal.nombre}  {this.state.tiendaModal.descripcion}
+        </div>
+      </div>
+    );
+  }
 }
 export default Mapa;
