1c1359a3a26e70e318198b5fbfc4e2d043c30dd9
Sebastián <sebastian.arrue@pucp.pe>
2019-10-19 01:31:26 -0500

    videolist embebida con altura hardcodeada

--- src/components/publicidad/Publicidad.js
+++ src/components/publicidad/Publicidad.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-class Publicidad extends React.Component{
+class Publicidad extends React.Component {
     // constructor(props) {
     //     super(props);
     // }
-    render (){
-        return(
-            <div>Publicidad
-   
+    render() {
+        return (
+            <div>
+                <iframe width="100%" height="500" src="https://www.youtube.com/embed/videoseries?list=PLC0w3lEHx2SF3NsbnqnLbWBWyF_3g0cjZ&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"></iframe>
             </div>
         )
     }
 }
-  
+
 export default Publicidad;
