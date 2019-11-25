f8a8e081d6eae1569e8971a4dc07d33b80fa8154
Sebastián Arrué Pajares <sebastian.arrue@pucp.pe>
2019-11-25 17:27:32 +0000

    Speed

--- src/components/juegos/ruleta/Ruleta.js
+++ src/components/juegos/ruleta/Ruleta.js
@@ -141,7 +141,8 @@ class Ruleta extends React.Component {
             } */
 
             deg += speed;
-            deg %= 360;
+            deg %= 360
+            console.log(speed);
 
             // Increment speed
             if (!isStopped && speed < 3) {
